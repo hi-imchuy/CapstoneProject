1f12187e2b1743a98a86e2f4787f33e9d04f45c8
unknown <chauhoanghuy562003@gmail.com>
2026-03-19 20:27:53 +0700

    init code

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button"
+
+function App() {
+  return (
+    <>
+      <Button variant="outlined">ABCD</Button>
+    </>
+  )
+}
+
+export default App;
---
+++ front-end/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  define: {
+    // eslint-disable-next-line no-undef
+    'process.env': process.env
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  // base: './'
+  resolve: {
+    alias: [
+      { find: '~', replacement: '/src' }
+    ]
+  }
+})
